18c84e17aecd0c6f2bbc20363596a62d84436ff3
Jordan Campbell <jcampbell1441@gmail.com>
2020-06-20 19:54:36 -0500

    Started

--- program.js
+++ program.js
@@ -1,12 +1,23 @@
 function onNodeInserted(event) {
   const element = document.querySelector('[aria-label="Timeline: Trending now"]');
   if (element !== null) {
-    console.log(element);
-
     element.style.display = 'none';
+  }
+
+  if (hasUpdated === false) {
+    const elements = document.querySelector('article').querySelectorAll('span');
+
+    console.log('Has updated was false!');
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].innerText = elements[i].innerText.replace('Trump', 'Stupid Little Twat');
+    }
+    console.log("Updated text");
+
+    hasUpdated = true;
   }
 }
 
+let hasUpdated = false;
 console.log("Bye bye trending!");
 
 document.addEventListener('DOMNodeInserted', onNodeInserted);
